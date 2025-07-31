72014a695541e68e0d594997fab8eec14a47ba4c
bishnoyvarun <bishnoy.varun@gmail.com>
2025-07-31 15:36:45 +0000

    added httpd image

--- docker_nginx.sh
+++ docker_nginx.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-cd /home/vakky/docker_image/
+#cd /home/vakky/docker_image/
 echo "**** Pulling Apache Image from Docker Hub ****"
-docker pull bishnoyvarun/apache
+docker pull bishnoyvarun/httpd
 
 echo "*** check running Containers***"
 docker ps -a
 
 echo "*** run the container***"
-docker run -itd -p 80:80 bishnoyvarun/apache
+docker run -itd -p 8081:80 bishnoyvarun/httpd
 
 echo "*** adding docker volume ***"
 docker volume create my_vol
